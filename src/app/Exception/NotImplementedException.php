b349cfa024ed51ad8eb684c9c29d2bf49e5fb97d
Pluveto <receding@protonmail.com>
2020-07-29 22:36:37 +0800

    fuck

--- src/app/Exception/NotImplementedException.php
+++
@@ -1,8 +0,0 @@
-<?php
-class NotImplementedException extends BadMethodCallException
-{
-    public function __construct($message = "Not implemented!")
-    {
-        parent::__construct($message);
-    }
-}
